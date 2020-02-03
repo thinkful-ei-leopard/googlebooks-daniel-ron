ba258948b2978f78f9701e8fec27e18859590cd9
Ron Martin <ronmartin89@gmail.com>
2020-02-03 14:31:30 -0800

    removed pointless comments

--- src/App.js
+++ src/App.js
@@ -28,25 +28,16 @@ class App extends React.Component {
     })
   }
   
-  // If search = DOG with no filters
-  // URL?Q=DOG
-
-  // if search = '' with printType = books
-  // URL shouldnt do anything
-  
-
   handleDropdownSelection = (selected) => {
     console.log(selected);
     if (this.state.searchInput !== '') {
       let res = selected.charAt(0);
       if (res === 'p') {
-        // console.log(`printType: ${selected}`);
         this.setState({
           printType: selected
         });
         
       } else {
-        // console.log(`filter ${selected}`);
         this.setState({
           filter: selected,
         });
@@ -62,7 +53,6 @@ class App extends React.Component {
       .then(resJson => this.setState( {items: resJson.items,})
       );
     }
-    // this.setState({printType: , bookType: })
   };
 
   handleSearchClick = (searchInput) => {
@@ -71,16 +61,12 @@ class App extends React.Component {
     // use DATA to make GET request
     // with that RESP, update state
     console.log(searchInput);
-    // GET https://www.googleapis.com/books/v1/volumes?q={search terms}
-    // https://www.googleapis.com/books/v1/volumes?q=Test&filter=ebooks
-    //printType = &filter=WHATEVERTHEVALUEIS
     const URL = `https://www.googleapis.com/books/v1/volumes?q=${searchInput}${this.state.printType}${this.state.bookType}&key=AIzaSyA26sUh6FAvgtPAuU3gtCpzGBdN7Yyd5IU`;
     console.log(URL);
     fetch(URL)
       .then(res => res.json())
       .then(resJson => this.setState( {items: resJson.items, searchInput: searchInput })
       );
-    // update state
   };
   
   // componentDidMount() {
